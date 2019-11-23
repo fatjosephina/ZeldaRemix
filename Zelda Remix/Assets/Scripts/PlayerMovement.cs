2d04bc42a9014e73ed3615a7090f5ac297194810
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2019-11-23 00:03:25 -0600

    Created player and animations

---
+++ Zelda Remix/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 positionChange;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionChange = Vector3.zero;
+        positionChange.x = Input.GetAxisRaw("Horizontal");
+        positionChange.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (positionChange != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", positionChange.x);
+            animator.SetFloat("moveY", positionChange.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + positionChange * speed * Time.deltaTime);
+    }
+}
