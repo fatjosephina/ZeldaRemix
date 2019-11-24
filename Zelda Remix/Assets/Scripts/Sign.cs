bcf2213782b5a96090a98fccc55ce681d12aaacc
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2019-11-24 00:10:29 -0600

    Added sign

---
+++ Zelda Remix/Assets/Scripts/Sign.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public Text dialogueText;
+    public string dialogue;
+    public bool isPlayerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isPlayerInRange == true)
+        {
+            if (dialogueBox.activeInHierarchy)
+            {
+                dialogueBox.SetActive(false);
+            }
+            else
+            {
+                dialogueBox.SetActive(true);
+                dialogueText.text = dialogue;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            dialogueBox.SetActive(false);
+        }
+    }
+}
