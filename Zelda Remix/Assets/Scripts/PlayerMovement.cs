df94d34d9549ba48f87b53580014f9a688ece8ba
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2019-11-25 03:26:23 -0600

    Added enemy

--- Zelda Remix/Assets/Scripts/PlayerMovement.cs
+++ Zelda Remix/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 positionChange;
@@ -12,8 +20,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -22,7 +33,24 @@ public class PlayerMovement : MonoBehaviour
         positionChange = Vector3.zero;
         positionChange.x = Input.GetAxisRaw("Horizontal");
         positionChange.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        if (Input.GetButtonDown("Fire1") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
     }
 
     void UpdateAnimationAndMove()
@@ -42,6 +70,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveCharacter()
     {
+        positionChange.Normalize();
         myRigidbody.MovePosition(transform.position + positionChange * speed * Time.deltaTime);
     }
 }
